2faeecffba707aea5536b8311451cd7b03eb90a0
Rishabh Kar <rk.official22@gmail.com>
2025-12-23 00:52:51 +0530

    Initial Commit

---
+++ src/main/java/com/prarambh/act/one/ActOneApplication.java
@@ -0,0 +1,13 @@
+package com.prarambh.act.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prarambh/act/one/ActOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prarambh.act.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
